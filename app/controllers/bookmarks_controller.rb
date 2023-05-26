4605fcd11954897f13b46e063070bab510c70985
TomKim <ehdgus5289@gmail.com>
2023-05-26 16:03:13 -0400

    route done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,21 @@
 class BookmarksController < ApplicationController
-
   def new
     @bookmark = Bookmark.new
-    @lists = List.find(params[:list_id])
+    @list = List.find(params[:list_id])
   end
 
+  def create
+    # raise
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(comment: params[:bookmark][:comment], list: @list, movie: @movie)
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment)
+  end
 end
